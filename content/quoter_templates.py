c14154e5c8424f7c1411516e417d185ac8ac3917
alperenonat <alprnont@gmail.com>
2024-10-14 00:56:06 +0200

    .

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Inloggen week 3 Alperen!!!</label>
+<label class="link" for="signinCheckbox">Inloggen week 3 Alperen!!!!</label>
 """
 
     return f"""<!DOCTYPE html>
